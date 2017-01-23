291cd24213db75f29b0a7e03f74144925fd8eae9
Quendal <quendal@gmail.com>
2017-01-23 15:08:08 -0600

    Cambios a subir a la rama hotfix-mail

--- app/src/main/java/com/grupotrt/plataforma/App.java
+++ app/src/main/java/com/grupotrt/plataforma/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!!" );
-
+        System.out.println( "Hola Mundo!! hotfix" );
     }
 }
