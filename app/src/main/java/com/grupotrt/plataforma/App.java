8c6431431e4c5d93b21492107a40f68155325e5c
Quendal <quendal@gmail.com>
2017-01-23 14:27:28 -0600

    Primera modificación

---
+++ app/src/main/java/com/grupotrt/plataforma/App.java
@@ -0,0 +1,14 @@
+package com.grupotrt.plataforma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo!!" );
+
+    }
+}
