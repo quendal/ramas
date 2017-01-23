e5b08f4c1b11f55bfc1caba8a7b9031b87e0c54c
Quendal <quendal@gmail.com>
2017-01-23 15:38:00 -0600

    finalización de funcionalidad

--- app/src/main/java/com/grupotrt/plataforma/App.java
+++ app/src/main/java/com/grupotrt/plataforma/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!!" );
+        System.out.println( "Hola Mundo!! develop" );
 
     }
 }
